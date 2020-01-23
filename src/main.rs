c9be7bf1ffa51b3280e37634486d4f8b8c427393
John Newby <john@newby.org>
2020-01-23 17:22:12 +0100

    Made audio playing work, now put stuff back in

--- src/main.rs
+++ src/main.rs
@@ -2,31 +2,8 @@ use clap::{App, Arg};
 use hound;
 #[macro_use]
 extern crate lazy_static;
+use crossbeam_channel::{unbounded, Receiver, Sender};
 use parking_lot::Mutex;
-use rustfft::num_complex::Complex;
-use rustfft::num_traits::Zero;
-use rustfft::FFTplanner;
-use std::cell::RefCell;
-use std::io::Write;
-use std::sync::mpsc::{channel, Receiver, Sender};
-use std::sync::Arc;
-use twang::Sound;
-
-struct SenderReceiver {
-    pub sender: Sender<f32>,
-    pub receiver: Receiver<f32>,
-}
-
-impl SenderReceiver {
-    pub fn new() -> Self {
-        let (sender, receiver) = channel();
-        Self { sender, receiver }
-    }
-}
-
-lazy_static! {
-    static ref SAMPLE_CHANNEL: Mutex<SenderReceiver> = Mutex::new(SenderReceiver::new());
-}
 
 const SAMPLE_RATE: usize = 48000;
 
@@ -57,24 +34,21 @@ fn main() {
         .register_port("speaker_r", jack::AudioOut::default())
         .expect("Error getting output device");
 
-    let (sender, receiver) = multiqueue::broadcast_queue(100);
-    let safe_sender = std::sync::Arc::new(std::sync::Mutex::new(sender));
-    let recv_stream = receiver.add_stream();
-
-    let sender_clone = safe_sender.clone();
+    let (sender, receiver) = unbounded::<f32>();
+    //let safe_sender = std::sync::Arc::new(std::sync::Mutex::new(sender));
 
     let jack_callback = move |_: &jack::Client, ps: &jack::ProcessScope| -> jack::Control {
-        let out = speaker_l.as_mut_slice(ps);
-        {
-            let receiver = &SAMPLE_CHANNEL.lock().receiver;
-            for v in out.iter_mut() {
-                if let Ok(sample) = receiver.recv() {
-                    *v = sample;
-                } else {
-                    sender_clone.lock().unwrap().try_send(None);
-                }
-            }
+        let out_l = speaker_l.as_mut_slice(ps);
+        let out_r = speaker_r.as_mut_slice(ps);
+
+        let stereo_out = out_l.iter_mut().zip(out_r.iter_mut());
+        for (left, right) in stereo_out {
+            *left = receiver.recv().unwrap();
+            *right = receiver.recv().unwrap();
         }
+        jack::Control::Continue
+    };
+    /*
         let data = jack_mic.as_slice(ps);
         let d2 = data.to_vec();
         match sender_clone.lock().unwrap().try_send(Some(d2)) {
@@ -84,8 +58,7 @@ fn main() {
                 jack::Control::Quit
             }
         }
-    };
-
+    */
     let _process = jack::ClosureProcessHandler::new(jack_callback);
     let active_client = client.activate_async((), _process).unwrap();
 
@@ -102,28 +75,17 @@ fn main() {
         .connect_ports_by_name("microphone_test:speaker_r", "system:playback_2")
         .unwrap();
 
-    std::thread::spawn(|| {
+    let wav_reader = std::thread::spawn(move || {
         for sample in hound::WavReader::new(std::io::stdin())
             .unwrap()
             .samples::<f32>()
         {
-            SAMPLE_CHANNEL.lock().sender.send(sample.unwrap() as f32);
+            sender.send(sample.unwrap());
         }
     });
 
     println!("After activate async");
-    let clone = recv_stream.clone();
-    receiver.unsubscribe();
-    while let Ok(received) = clone.recv() {
-        match received {
-            Some(audio_buffer) => {
-                println!("Read {}", audio_buffer.len());
-            }
-            None => break,
-        }
-    }
+    wav_reader.join();
     active_client.deactivate().unwrap();
-    drop(safe_sender.lock());
-    println!("Waiting for threads to exit");
     println!("Done");
 }
